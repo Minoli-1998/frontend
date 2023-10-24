b75833f3706d6bbc5e39028c335a44f6711d45ad
Minoli <mbhagyagunathilake@gmail.com>
2023-10-24 20:55:03 +0530

    changes added to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,56 @@
 import React, { useState } from "react";
 import { images } from "../constants";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const navItemInfo = [
-  { name: "Home" },
-  { name: "Articles" },
-  { name: "Pages" },
-  { name: "Pricing" },
-  { name: "Faq" },
+  { name: "Home", type: "link" },
+  { name: "Articles", type: "link" },
+  { name: "Pages", type: "dropdown", items: ["About Us", "Contact Us"] },
+  { name: "Pricing", type: "link" },
+  { name: "Faq", type: "link" },
 ];
 
-const NavItems = ({ name }) => {
+const NavItems = ({ item }) => {
+  const [dropDown, setDropDown] = useState(false);
+
+  const toggleDropDownButton = () => {
+    setDropDown((curState) => {
+      return !curState;
+    });
+  };
+
   return (
     <li className="relative group">
-      <a href="/" className="px-4 py-2">
-        {name}
-      </a>
-      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 opacity-0 group-hover:right-[90%] group-hover:opacity-100">
-        /
-      </span>
+      {item.type === "link" ? (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className=" cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 opacity-0 group-hover:right-[90%] group-hover:opacity-100">
+            /
+          </span>
+        </>
+      ) : (
+        <div className="flex flex-col items-center">
+          <button className="px-4 py-2 flex gap-x-1 items-center">
+            <span>{item.name}</span>
+            <MdOutlineKeyboardArrowDown />
+          </button>
+          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
+            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items.map((page) => (
+                <a
+                  href="/"
+                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                >
+                  {page}
+                </a>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </li>
   );
 };
@@ -55,7 +87,7 @@ const Header = () => {
         >
           <ul className="text-white lg:text-dark-soft items-center gap-y-5 flex flex-col lg:flex-row gap-x-2 font-semibold">
             {navItemInfo.map((item) => (
-              <NavItems key={item.name} name={item.name} />
+              <NavItems key={item.name} item={item} />
             ))}
           </ul>
           <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 text-blue-500 font-semibold hover:bg-blue-500 hover:text-white rounded-full transition-all duration-300">
